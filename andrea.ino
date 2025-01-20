bc1b1b212f7b03a746ccc4231803cf0ab761d4de
Atha’illah Shidqii N I <athaillah396@gmail.com>
2025-01-20 16:22:01 +0700

    andrea.ino
    logika utama (looping dan tindakan sistem)

--- andrea.ino
+++ andrea.ino
@@ -1,37 +1,20 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-#include "base64.h"
+void loop() {
+  // Membaca status sensor
+  int sensorState = digitalRead(sensorPin);
 
-// Setup WiFi network
-const char* ssid = "xxxxxxxxxxxx";
-const char* password = "xxxxxxxxxx";
-
-// read sensor
-const int sensorPin = A0;
-int speakerPin = D5;
-int state = 0;
-
-// Twilio Parameter
-String account_sid = "Axxxxxxxxxxxxxxxxxxxxxxxxxx";
-String auth_token = "8xxxxxxxxxxxxxxxxxxxxxxxxxx";
-String from = "14xxxxxxxxx";
-String to = "62xxxxxxxxxxxxxxx";
-String body = "Pintu Ada Yang Buka";
-
-void setup() {
- 
-  Serial.begin(115200);
- 
-  WiFi.begin(ssid, password);
-  
-  pinMode(sensorPin,INPUT);
-  pinMode(speakerPin, OUTPUT);
-  
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
+  // Mengecek apakah ada perubahan status pada sensor
+  if (sensorState != lastSensorState) {
+    lastSensorState = sensorState;
+    
+    if (sensorState == LOW) { 
+      Serial.println("PINTU ADA YANG BUKA!");
+      digitalWrite(buzzerPin, HIGH); 
+      bot.sendMessage(CHAT_ID, "PINTU ADA YANG BUKA!", "");
+    } else { 
+      Serial.println("Pintu tertutup.");
+      digitalWrite(buzzerPin, LOW); 
+      bot.sendMessage(CHAT_ID, "Pintu tertutup.", "");
+    }
   }
- 
-  Serial.println("Connected to WiFi");
- 
+  delay(500); 
 }
